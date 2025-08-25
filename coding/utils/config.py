1982f3232156c6ea065183747b47db8d83026683
brokespace <brokespace@nil.com>
2025-08-25 14:53:06 -0600

    upped the vpermit tao limit

--- coding/utils/config.py
+++ coding/utils/config.py
@@ -256,7 +256,7 @@ def add_validator_args(cls, parser):
         "--neuron.vpermit_tao_limit",
         type=int,
         help="The maximum number of TAO allowed to query a validator with a vpermit.",
-        default=4096,
+        default=20000,
     )
 
     parser.add_argument(
